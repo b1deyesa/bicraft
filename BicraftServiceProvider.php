7ee4337d0373f0e94d6e8fe759eaa387364cc95f
= <=>
2024-09-27 13:36:26 +0700

    Vite Config

--- BicraftServiceProvider.php
+++ BicraftServiceProvider.php
@@ -10,12 +10,27 @@ class BicraftServiceProvider extends ServiceProvider
     {
         $this->publishes([
             __DIR__.'/views' => resource_path('views'),
-            __DIR__.'/views' => resource_path('views'),
+            __DIR__.'/js' => resource_path('js'),
+            __DIR__.'/sass' => resource_path('sass'),
+            __DIR__.'/app' => resource_path('app'),
         ], 'bicraft');
+
+        $viteConfigPath = base_path('vite.config.js');
+        
+        if (file_exists($viteConfigPath)) {
+            $viteConfig = file_get_contents($viteConfigPath);
+            $updatedConfig = str_replace(
+                "['resources/css/app.css', 'resources/js/app.js']",
+                "['resources/sass/app.scss', 'resources/js/app.js']",
+                $viteConfig
+            );
+
+            file_put_contents($viteConfigPath, $updatedConfig);
+        }
     }
 
     public function register()
     {
-        // 
+        // Logic lainnya jika ada
     }
 }
